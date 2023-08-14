db5e5f8fb010561390de6650ea9feec81cf9d2b0
Success Charles <chizaramsuccess9@gmail.com>
2023-08-15 00:18:39 +0100

    fixed animation issue

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
 class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
   @override
-  _HomePageState createState() => _HomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
@@ -61,137 +64,174 @@ class _HomePageState extends State<HomePage> {
   Widget makePage({image, title, description, page}) {
     return Container(
       decoration: BoxDecoration(
-          image: DecorationImage(image: AssetImage(image), fit: BoxFit.cover)),
+        image: DecorationImage(image: AssetImage(image), fit: BoxFit.cover),
+      ),
       child: Container(
         decoration: BoxDecoration(
-            gradient: LinearGradient(begin: Alignment.bottomRight, stops: [
-          0.3,
-          0.9
-        ], colors: [
-          Colors.black.withOpacity(.9),
-          Colors.black.withOpacity(.2),
-        ])),
+          gradient: LinearGradient(
+            begin: Alignment.bottomRight,
+            stops: const [0.3, 0.9],
+            colors: [
+              Colors.black.withOpacity(.9),
+              Colors.black.withOpacity(.2),
+            ],
+          ),
+        ),
         child: Padding(
-          padding: EdgeInsets.all(20),
+          padding: const EdgeInsets.all(20),
           child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                SizedBox(
-                  height: 40,
-                ),
-                Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const SizedBox(
+                height: 40,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.baseline,
+                textBaseline: TextBaseline.alphabetic,
+                children: <Widget>[
+                  Text(
+                    page.toString(),
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    '/$totalPage'.toString(),
+                    style: const TextStyle(color: Colors.white, fontSize: 15),
+                  )
+                ],
+              ),
+              Expanded(
+                child: Column(
                   mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.baseline,
-                  textBaseline: TextBaseline.alphabetic,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Text(
-                      page.toString(),
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      '/' + totalPage.toString(),
-                      style: TextStyle(color: Colors.white, fontSize: 15),
-                    )
-                  ],
-                ),
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        title,
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 50,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Row(
-                        children: <Widget>[
-                          Container(
-                            margin: EdgeInsets.only(right: 3),
-                            child: Icon(
-                              Icons.star,
-                              color: Colors.yellow,
-                              size: 15,
+                    AnimationLimiter(
+                      child: AnimationConfiguration.staggeredList(
+                        position: 0,
+                        duration: const Duration(milliseconds: 500),
+                        child: SlideAnimation(
+                          duration: const Duration(milliseconds: 500),
+                          child: Text(
+                            title,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 50,
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
-                          Container(
-                            margin: EdgeInsets.only(right: 3),
-                            child: Icon(
-                              Icons.star,
-                              color: Colors.yellow,
-                              size: 15,
-                            ),
-                          ),
-                          Container(
-                            margin: EdgeInsets.only(right: 3),
-                            child: Icon(
-                              Icons.star,
-                              color: Colors.yellow,
-                              size: 15,
-                            ),
-                          ),
-                          Container(
-                            margin: EdgeInsets.only(right: 3),
-                            child: Icon(
-                              Icons.star,
-                              color: Colors.yellow,
-                              size: 15,
-                            ),
-                          ),
-                          Container(
-                            margin: EdgeInsets.only(right: 5),
-                            child: Icon(
-                              Icons.star,
-                              color: Colors.grey,
-                              size: 15,
-                            ),
-                          ),
-                          Text(
-                            '4.0',
-                            style: TextStyle(color: Colors.white70),
-                          ),
-                          Text(
-                            '(2300)',
-                            style:
-                                TextStyle(color: Colors.white38, fontSize: 12),
-                          )
-                        ],
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(right: 50),
-                        child: Text(
-                          description,
-                          style: TextStyle(
-                              color: Colors.white.withOpacity(.7),
-                              height: 1.9,
-                              fontSize: 15),
                         ),
                       ),
-                      SizedBox(
-                        height: 20,
+                    ),
+                    const SizedBox(height: 20.0),
+                    AnimationLimiter(
+                      child: AnimationConfiguration.staggeredList(
+                        position: 1,
+                        duration: const Duration(milliseconds: 500),
+                        child: SlideAnimation(
+                          duration: const Duration(milliseconds: 500),
+                          child: Row(
+                            children: <Widget>[
+                              Container(
+                                margin: const EdgeInsets.only(right: 3),
+                                child: const Icon(
+                                  Icons.star,
+                                  color: Colors.yellow,
+                                  size: 15,
+                                ),
+                              ),
+                              Container(
+                                margin: const EdgeInsets.only(right: 3),
+                                child: const Icon(
+                                  Icons.star,
+                                  color: Colors.yellow,
+                                  size: 15,
+                                ),
+                              ),
+                              Container(
+                                margin: const EdgeInsets.only(right: 3),
+                                child: const Icon(
+                                  Icons.star,
+                                  color: Colors.yellow,
+                                  size: 15,
+                                ),
+                              ),
+                              Container(
+                                margin: const EdgeInsets.only(right: 3),
+                                child: const Icon(
+                                  Icons.star,
+                                  color: Colors.yellow,
+                                  size: 15,
+                                ),
+                              ),
+                              Container(
+                                margin: const EdgeInsets.only(right: 5),
+                                child: const Icon(
+                                  Icons.star,
+                                  color: Colors.grey,
+                                  size: 15,
+                                ),
+                              ),
+                              const Text(
+                                '4.0',
+                                style: TextStyle(color: Colors.white70),
+                              ),
+                              const Text(
+                                '(2300)',
+                                style: TextStyle(
+                                  color: Colors.white38,
+                                  fontSize: 12,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
                       ),
-                      Text(
-                        'READ MORE',
-                        style: TextStyle(color: Colors.white),
+                    ),
+                    const SizedBox(height: 20.0),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 50),
+                      child: AnimationLimiter(
+                        child: AnimationConfiguration.staggeredList(
+                          position: 2,
+                          duration: const Duration(milliseconds: 500),
+                          child: SlideAnimation(
+                            duration: const Duration(milliseconds: 500),
+                            child: Text(
+                              description,
+                              style: TextStyle(
+                                  color: Colors.white.withOpacity(.7),
+                                  height: 1.9,
+                                  fontSize: 15),
+                            ),
+                          ),
+                        ),
                       ),
-                      SizedBox(
-                        height: 30,
+                    ),
+                    const SizedBox(height: 20.0),
+                    const AnimationLimiter(
+                      child: AnimationConfiguration.staggeredList(
+                        position: 3,
+                        duration: Duration(milliseconds: 500),
+                        child: SlideAnimation(
+                          duration: Duration(milliseconds: 500),
+                          child: Text(
+                            'READ MORE',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
                       ),
-                    ],
-                  ),
-                )
-              ]),
+                    ),
+                    const SizedBox(
+                      height: 30.0,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
